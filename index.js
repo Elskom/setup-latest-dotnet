fe12c9e9229b9424bdc32fa0928d18ff4c6ad398
AraHaan <seandhunt_7@yahoo.com>
2021-07-22 00:37:48 -0400

    Fixed issue where the updated SDK is not added to PATH and DOTNET_ROOT is not exported.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const https = require("https"),
     fs = require("fs"),
-    spawnSync = require("child_process").spawnSync
+    path = require("path"),
+    spawnSync = require("child_process").spawnSync,
+    core = require("@actions/core")
 
 class Action
 {
@@ -70,11 +72,30 @@ class Action
 
             // Windows.
             this._executeCommand(`${__dirname}/dotnet-install.ps1 -Channel ${this.versionMajor}.${this.versionMinor} -Quality daily`, { encoding: "utf-8", stdio: [process.stdin, process.stdout, process.stderr] })
+            if (!process.env['DOTNET_INSTALL_DIR'])
+            {
+                // This is the default set in install-dotnet.ps1
+                core.addPath(path.join(process.env['LocalAppData'] + '', 'Microsoft', 'dotnet'))
+                core.exportVariable('DOTNET_ROOT', path.join(process.env['LocalAppData'] + '', 'Microsoft', 'dotnet'))
+            }
         }
         else
         {
             // Linux and MacOS.
             this._executeCommand(`${__dirname}/dotnet-install.sh --channel ${this.versionMajor}.${this.versionMinor} --quality daily`, { encoding: "utf-8", stdio: [process.stdin, process.stdout, process.stderr] })
+            if (!process.env['DOTNET_INSTALL_DIR'])
+            {
+                // This is the default set in install-dotnet.sh
+                core.addPath(path.join(process.env['HOME'] + '', '.dotnet'))
+                core.exportVariable('DOTNET_ROOT', path.join(process.env['HOME'] + '', '.dotnet'))
+            }
+        }
+
+        // Add DOTNET_INSTALL_DIR to path and export the variable DOTNET_ROOT if DOTNET_INSTALL_DIR is set.
+        if (process.env['DOTNET_INSTALL_DIR'])
+        {
+            core.addPath(process.env['DOTNET_INSTALL_DIR'])
+            core.exportVariable('DOTNET_ROOT', process.env['DOTNET_INSTALL_DIR'])
         }
     }
 }
