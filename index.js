32930107d2fc27b850169dfe99dc0301787928e5
AraHaan <seandhunt_7@yahoo.com>
2021-12-22 01:45:33 -0500

    Fixed issue where action downloads the install script but fails to run it. (#3)

--- index.js
+++ index.js
@@ -25,52 +25,10 @@ class Action
         return spawnSync(TOOL, ARGS, options)
     }
 
-    downloadInstallScript(url, dest)
-    {
-        return new Promise((resolve) => {
-            const file = fs.createWriteStream(dest, { flags: "wx" })
-            const request = https.get(url, response => {
-                if (response.statusCode === 200) {
-                    response.pipe(file)
-                } else {
-                    file.close()
-                    fs.unlink(dest, () => {}) // Delete temp file
-                    this._printErrorAndExit(`Server responded with ${response.statusCode}: ${response.statusMessage}`)
-                }
-            })
-
-            request.on("error", err => {
-                file.close()
-                fs.unlink(dest, () => {}) // Delete temp file
-                this._printErrorAndExit(err.message)
-            })
-
-            file.on("finish", () => {
-                resolve()
-            })
-
-            file.on("error", err => {
-                file.close()
-                if (err.code === "EEXIST") {
-                    this._printErrorAndExit("File already exists")
-                } else {
-                    fs.unlink(dest, () => {}) // Delete temp file
-                    this._printErrorAndExit(err.message)
-                }
-            })
-        })
-    }
-
     run()
     {
         if (process.platform === 'win32')
         {
-            console.log('Downloading .NET Install script.')
-
-            // Download install script first for Windows.
-            this.downloadInstallScript('https://raw.githubusercontent.com/dotnet/install-scripts/main/src/dotnet-install.ps1', `${__dirname}/dotnet-install.ps1`)
-            console.log('Download Complete.')
-
             // Windows.
             this._executeCommand(`${__dirname}/dotnet-install.ps1 -Channel ${this.versionMajor}.${this.versionMinor}.${this.versionBand} -Quality daily`, { encoding: "utf-8", stdio: [process.stdin, process.stdout, process.stderr] })
             if (!process.env['DOTNET_INSTALL_DIR'])
